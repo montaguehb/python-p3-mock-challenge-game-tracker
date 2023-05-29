c2a3d16ff2876d5cbf85c57f209708113c5fc0a3
Historia Montague <historia.montague@outlook.com>
2023-05-29 11:27:34 -0600

    Finished Lab

--- lib/classes/game.py
+++ lib/classes/game.py
@@ -1,16 +1,35 @@
 class Game:
+    all = []
+    
     def __init__(self, title):
-        self.title = title
+        if(len(title)):
+            self._title = title
+        else:
+            raise AttributeError
         self._results = []
         self._players = []
-        
+        Game.all.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        raise AttributeError
+         
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if(new_result):
+            self._results.append(new_result)
+        return [result for result in Result.all if result.game == self]
     
     def players(self, new_player=None):
         from classes.player import Player
-        pass
+        if(new_player):
+            self._players.append(new_player)
+        return self._players
     
     def average_score(self, player):
-        pass
+        all_scores = [result.score for result in self._results if result.player == player]
+        return sum(all_scores)/len(all_scores)
--- lib/classes/player.py
+++ lib/classes/player.py
@@ -6,20 +6,36 @@ class Player:
         self.username = username
         self._results = []
         self._games_played = []
-        
+        Player.all.append(self)
+    
+    @property
+    def username(self):
+        return self._username
+    
+    @username.setter
+    def username(self, username):
+            if(2 <= len(username) <= 16):
+                self._username = username
+            else:
+                raise AttributeError
+            
     def results(self, new_result=None):
         from classes.result import Result
-        pass
+        if(new_result):
+            self._results.append(new_result)
+        return [result for result in Result.all if result.player == self]
     
     def games_played(self, new_game=None):
         from classes.game import Game
-        pass
+        if(new_game):
+            self._games_played.append(new_game)
+        return [game for game in Game.all if self in game._players]
     
     def played_game(self, game):
-        pass
+        return game in self._games_played
     
     def num_times_played(self, game):
-        pass
+        return len([played for played in self._games_played if game == played])
     
     @classmethod
     def highest_scored(cls, game):
